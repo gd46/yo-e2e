2b04125c2f55f432b97217c700a4fa71bd3f0755
gd46 <gd46@njit.edu>
2016-08-31 23:01:35 -0400

    Added team prompt and validation for user input

--- generators/page-object/index.js
+++ generators/page-object/index.js
@@ -1,16 +1,26 @@
 var generators = require('yeoman-generator');
 var _ = require('lodash');
 module.exports = generators.Base.extend({
- 
+
   prompting: function () {
     return this.prompt([{
       type    : 'input',
       name    : 'name',
-      message : 'Your project name',
+      message : 'Your page-object name',
       default : this.appname // Default to current folder name
+    }, {
+      type  : 'input',
+      name  : 'team',
+      message : 'The team folder to create it in',
+      validate: function (input) {
+        if(_.isUndefined(input) || _.isNull(input) || input.trim() === '') {
+          return 'Please define a team folder for the page object';
+        } else {
+           return true;
+        }
+      }
     }]).then(function (answers) {
     	this.props = answers;
-      this.log('app name', answers.name);
     }.bind(this));
   },
 
@@ -18,9 +28,10 @@ module.exports = generators.Base.extend({
   	config: function () {
       var camelCaseName = _.camelCase(this.props.name);
       var kebabCaseName = _.kebabCase(this.props.name);
+
     	this.fs.copyTpl(
               this.templatePath('_template-page.js'),
-              this.destinationPath('test/page_objects/'+ kebabCaseName +'-page.js'), {
+              this.destinationPath('test/page_objects/' + this.props.team + '/' + kebabCaseName +'-page.js'), {
               	camelCaseName: camelCaseName
               }
           );
