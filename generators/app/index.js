0dfc4c9a09ecfdd0a0fefb687b41290f3060ea29
gd46 <gd46@njit.edu>
2016-09-10 14:21:00 -0400

    Renamed template files to be prefixed with an underscore

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,20 +4,20 @@ module.exports = generators.Base.extend({
 
   initializing: function () {
     // Set up automation framework files and folders
-    this.copy("env.js", "test/support/env.js");
-    this.copy("hooks.js", "test/support/hooks.js");
+    this.copy("_env.js", "test/support/env.js");
+    this.copy("_hooks.js", "test/support/hooks.js");
 
-    this.copy("expected_conditions.js", "test/includes/expected_conditions.js");
-    this.copy("external_globals.js", "test/includes/external_global.js");
-    this.copy("internal_globals.js", "test/includes/internal_globals.js");
-    this.copy("page_objects.js", "test/includes/page_objects.js");
+    this.copy("_expected_conditions.js", "test/includes/expected_conditions.js");
+    this.copy("_external_globals.js", "test/includes/external_global.js");
+    this.copy("_internal_globals.js", "test/includes/internal_globals.js");
+    this.copy("_page_objects.js", "test/includes/page_objects.js");
 
     
-    this.copy("cucumber.conf.js", "cucumber.conf.js");
+    this.copy("_cucumber.conf.js", "cucumber.conf.js");
     var packagejsonExists = this.fs.exists('package.json');
     if(!packagejsonExists) {
         this.fs.copyTpl(
-              this.templatePath('package.json'),
+              this.templatePath('_package.json'),
               this.destinationPath('package.json')
           );
     //     console.log('No package.json');
