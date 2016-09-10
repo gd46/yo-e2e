7e7e94ef8bd8dac28529fb3bbeb2f513493718dd
gd46 <gd46@njit.edu>
2016-09-10 14:18:52 -0400

    Added test framework install dependencies

--- generators/app/index.js
+++ generators/app/index.js
@@ -16,24 +16,28 @@ module.exports = generators.Base.extend({
     this.copy("cucumber.conf.js", "cucumber.conf.js");
     var packagejsonExists = this.fs.exists('package.json');
     if(!packagejsonExists) {
-        // this.fs.copyTpl('package.json', 'package.json');
-        // this.fs.copyTpl(
-        //       this.templatePath('package.json'),
-        //       this.destinationPath('package.json')
-        //   );
-        console.log('No package.json');
-        shell.exec('npm init', function(err, stdout, stderr) {
-          if (err) {
-            console.error('error',err);
-            return;
-          }
-          console.log('stderr', stderr);
-          console.log('success',stdout);
-        });
+        this.fs.copyTpl(
+              this.templatePath('package.json'),
+              this.destinationPath('package.json')
+          );
+    //     console.log('No package.json');
+    //     shell.exec('npm init', function(err, stdout, stderr) {
+    //       if (err) {
+    //         console.error('error',err);
+    //         return;
+    //       }
+    //       console.log('stderr', stderr);
+    //       console.log('success',stdout);
+    //     });
     }
   },
 
   install: function () {
-     // this.npmInstall(['cucumber'], { 'saveDev': true });
+     this.npmInstall(['chai@^3.5.0'], { 'saveDev': true });
+     this.npmInstall(['chai-as-promised@^5.3.0'], { 'saveDev': true });
+     this.npmInstall(['lodash@^4.13.1'], { 'saveDev': true });
+     this.npmInstall(['cucumber@1.3.0'], { 'saveDev': true });
+     this.npmInstall(['protractor@3.3.0'], { 'saveDev': true });
+     this.npmInstall(['protractor-cucumber-framework@^0.6.0'], { 'saveDev': true });
   },
 });
