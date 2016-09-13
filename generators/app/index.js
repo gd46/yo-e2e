e525944d527035fbee0185990712c0e32f32a06e
Oscar Rieken <oscar.rieken@adp.com>
2016-09-13 17:09:57 -0400

    :beers: added note about using yosay and chalk, noted we should write package.json and not care but only on a new project

--- generators/app/index.js
+++ generators/app/index.js
@@ -25,29 +25,32 @@ module.exports = generators.Base.extend({
 
     // TODO: we should just write the file and not care if there is one there
     var packagejsonExists = this.fs.exists('package.json');
+
     if(!packagejsonExists) {
       this.fs.copyTpl(
           this.templatePath('_package.json'),
           this.destinationPath('package.json')
       );
-        console.log('No package.json');
-        shell.exec('npm init', function(err, stdout, stderr) {
-          if (err) {
-            console.error('error',err);
-            return;
-          }
-          console.log('stderr', stderr);
-          console.log('success',stdout);
-        });
+
+      // if we want to output anything we should use yosay and chalk
+      console.log('No package.json');
+      shell.exec('npm init', function (err, stdout, stderr) {
+        if (err) {
+          console.error('error', err);
+          return;
+        }
+        console.log('stderr', stderr);
+        console.log('success', stdout);
+      });
+    }
   },
 
   install: function () {
-  //  TODO: Write tests?
     // this.npmInstall(['chai@^3.5.0'], {'saveDev': true});
     // this.npmInstall(['chai-as-promised@^5.3.0'], {'saveDev': true});
     // this.npmInstall(['lodash@^4.13.1'], {'saveDev': true});
     // this.npmInstall(['cucumber@1.3.0'], {'saveDev': true});
     // this.npmInstall(['protractor@3.3.0'], {'saveDev': true});
     // this.npmInstall(['protractor-cucumber-framework@^0.6.0'], {'saveDev': true});
-  },
+  }
 });
