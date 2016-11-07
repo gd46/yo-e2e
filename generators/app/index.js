66bde797e71cb8953b71eb521f53573b8a88cf33
gd46 <gd46@njit.edu>
2016-11-06 20:43:44 -0500

    Renamed what external globals file gets created as

--- generators/app/index.js
+++ generators/app/index.js
@@ -23,13 +23,13 @@ module.exports = generators.Base.extend({
   //     this.npmInstall(['e2e_framework@https://github.com/gd46/e2e_framework']);
   //   }
   // },
-  end: function () {
+  initializing: function () {
 
     this.copy("env.js", "test/e2e/features/support/env.js");
     this.copy("hooks.js", "test/e2e/features/support/hooks.js");
 
     this.copy("expected_conditions.js", "test/e2e/includes/expected_conditions.js");
-    this.copy("external_globals.js", "test/e2e/includes/external_global.js");
+    this.copy("external_globals.js", "test/e2e/includes/external_globals.js");
     this.copy("internal_globals.js", "test/e2e/includes/internal_globals.js");
     this.copy("page_objects.js", "test/e2e/includes/page_objects.js");
 
@@ -40,5 +40,13 @@ module.exports = generators.Base.extend({
     this.copy("util.js", "test/e2e/utilities/util.js");
 
     this.copy("cucumber.conf.js", "cucumber.conf.js");
+  },
+  install: function () {
+    this.npmInstall(['chai@^3.5.0'], {'saveDev': true});
+    this.npmInstall(['chai-as-promised@^5.3.0'], {'saveDev': true});
+    this.npmInstall(['lodash@^4.13.1'], {'saveDev': true});
+    this.npmInstall(['cucumber@1.3.0'], {'saveDev': true});
+    this.npmInstall(['protractor@3.3.0'], {'saveDev': true});
+    this.npmInstall(['protractor-cucumber-framework@^0.6.0'], {'saveDev': true});
   }
 });
